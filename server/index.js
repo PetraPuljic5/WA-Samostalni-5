32b3d8d421a8090b0af5c97391e743598019a04a
Petra Puljić <ppuljicc@gmail.com>
2024-12-09 18:20:31 +0100

    express

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+app.get('/', (req, res) => {
+    res.send('Pizzeria');
+});
+
+app.listen(PORT, error => {
+    if (error) {
+        console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
+    } else {
+    console.log(`Server dela na http://localhost:${PORT}`);
+    }
+});

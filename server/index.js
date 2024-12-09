f621b63a0699f2e80578e7c7d315aa942a1e6bba
Petra Puljić <ppuljicc@gmail.com>
2024-12-09 19:11:48 +0100

    MongoDB

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import { connectToDatabase } from './db.js';
+
 const app = express();
+
+let db = await connectToDatabase();
 
 app.use(express.json());
 
 const PORT = 3000;
+
 app.get('/', (req, res) => {
     res.send('Pizzeria');
 });
+
+app.get('/pizze', async (req, res) => {
+    let pizze_collection = db.collection('pizze'); 
+    let allPizze = await pizze_collection.find().toArray();
+    res.status(200).json(allPizze);
+    });
 
 app.listen(PORT, error => {
     if (error) {

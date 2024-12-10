2401a19c094557257d608d75ebd22239511bd18c
Petra Puljić <ppuljicc@gmail.com>
2024-12-10 19:47:22 +0100

    POST
    1. dio zadatka gotov

--- server/index.js
+++ server/index.js
@@ -18,6 +18,25 @@ app.get('/pizze', async (req, res) => {
     res.status(200).json(allPizze);
     });
 
+app.post('/pizze', async (req, res) => {
+    const { naziv, cijena, slika, sastojci } = req.body;
+    
+    if (!naziv || typeof naziv !== 'string' || !cijena || typeof cijena !== 'number' || !slika || typeof slika !== 'string' || !Array.isArray(sastojci) || sastojci.some(s => typeof s !== 'string')) {
+        return res.status(400).json({ error: 'Podaci su neispravni.' });
+    }
+    
+    try {
+        let pizze_collection = db.collection('pizze');
+        const nova = { naziv, cijena, slika, sastojci };
+        const rez = await pizze_collection.insertOne(nova);
+        res.status(201).json(rez);
+    } catch (error) {
+        console.error('Greška u dodavanju', error);
+        res.status(500).json({ error: 'Greška.' });
+        }
+});
+    
+
 app.listen(PORT, error => {
     if (error) {
         console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
